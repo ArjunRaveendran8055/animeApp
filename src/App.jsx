4aa26ddce92aaa9f25085fde95cff54e9f61e83b
Arjun Raveendran <arjunravi1213@gmail.com>
2023-09-02 01:02:36 +0530

    task achived without proper designing

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ class App extends React.Component {
     super(props);
 
     this.state = { 
-      input:"",
-      images: [],
+      filteredImages:[],
+      images: []
     };
   }
   componentDidMount() {
@@ -16,20 +16,29 @@ class App extends React.Component {
       //console.log("response", res.data.results); //this is the response object that contains all the info we need to display
       const data=res.data.results
       //console.log("data is",data)
-         
+      this.setState({filteredImages:data})
       this.setState({ images: data });
     });
   }
 
   render() {
+
+    const inputChangeHandler=(e)=>{
+        this.setState({filteredImages:this.state.images.filter((item)=>{
+          if(item.name.includes(e.target.value)){
+            return item
+          }
+          console.log(this.state.filteredImages)
+        })})
+    }
+  
     return (
       <div className="main-container">
       <div className="search-container">
-      {console.log("input is",this.state.input)}
-      <input type="text" className="searchbar" placeholder="Search Pokemon...." onChange={(e)=>{this.setState({input:e.target.value})}}/>
+      <input type="text" className="searchbar" placeholder="Search Pokemon...." onChange={inputChangeHandler}/>
       </div>
         <div className="grid-container">
-          {this.state.images.map((item,index)=>{
+          {this.state.filteredImages.map((item,index)=>{
             return(
               <div className="card" key={index}>
               <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${index+1  }.png`} />
