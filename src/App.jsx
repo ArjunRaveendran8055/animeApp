b7676ef7b31db91d2cf220671d584ec20d3943fd
Arjun Raveendran <arjunravi1213@gmail.com>
2023-09-03 17:04:53 +0530

    only media query left

--- src/App.jsx
+++ src/App.jsx
@@ -6,44 +6,55 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
-      filteredImages:[],
-      images: []
+    this.state = {
+      filteredImages: [],
+      images: [],
     };
   }
   componentDidMount() {
     axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
       //console.log("response", res.data.results); //this is the response object that contains all the info we need to display
-      const data=res.data.results
+      const data = res.data.results;
       //console.log("data is",data)
-      this.setState({filteredImages:data})
+      this.setState({ filteredImages: data });
       this.setState({ images: data });
     });
   }
 
   render() {
-
-    const inputChangeHandler=(e)=>{
-        this.setState({filteredImages:this.state.images.filter((item)=>{
-          if(item.name.includes(e.target.value)){
-            return item
+    const inputChangeHandler = (e) => {
+      this.setState({
+        filteredImages: this.state.images.filter((item) => {
+          if (item.name.includes(e.target.value)) {
+            return item;
           }
-          console.log(this.state.filteredImages)
-        })})
-    }
-  
+          console.log(this.state.filteredImages);
+        }),
+      });
+    };
+
     return (
       <div className="main-container">
-      <div className="search-container">
-      <input type="text" className="searchbar" placeholder="Search Pokemon...." onChange={inputChangeHandler}/>
-      </div>
+        <div className="search-container">
+          <input
+            type="text"
+            className="searchbar"
+            placeholder="Search Pokemon...."
+            onChange={inputChangeHandler}
+          />
+        </div>
         <div className="grid-container">
-          {this.state.filteredImages.map((item,index)=>{
-            return(
+          {this.state.filteredImages.map((item, index) => {
+            return (
               <div className="card" key={index}>
-              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${index+1  }.png`} />
-              <p className="pokename">{item.name}</p>
-              </div>)
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${
+                    index + 1
+                  }.png`}
+                />
+                <p className="pokename">{item.name}</p>
+              </div>
+            );
           })}
         </div>
       </div>
