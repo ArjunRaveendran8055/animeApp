d6bb10f03e22affcca409bb2a2cfd7f41734d6df
Arjun Raveendran <arjunravi1213@gmail.com>
2023-09-03 22:58:22 +0530

    final touchup remaining

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      filteredImages: [],
+      input: "",
       images: [],
     };
   }
@@ -16,22 +16,16 @@ class App extends React.Component {
       //console.log("response", res.data.results); //this is the response object that contains all the info we need to display
       const data = res.data.results;
       //console.log("data is",data)
-      this.setState({ filteredImages: data });
       this.setState({ images: data });
     });
   }
 
   render() {
     const inputChangeHandler = (e) => {
-      this.setState({
-        filteredImages: this.state.images.filter((item) => {
-          if (item.name.includes(e.target.value)) {
-            return item;
-          }
-          console.log(this.state.filteredImages);
-        }),
-      });
+      this.setState({ input: e.target.value });
     };
+
+    let { input, images } = this.state;
 
     return (
       <div className="main-container">
@@ -44,18 +38,24 @@ class App extends React.Component {
           />
         </div>
         <div className="grid-container">
-          {this.state.filteredImages.map((item, index) => {
-            return (
-              <div className="card" key={index}>
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${
-                    index + 1
-                  }.png`}
-                />
-                <p className="pokename">{item.name}</p>
-              </div>
-            );
-          })}
+          {images
+            .filter((item, i) => {
+              if (item.name.includes(input)) {
+                return item;
+              }
+            })
+            .map((item, index) => {
+              return (
+                <div className="card" key={index}>
+                  <img
+                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${
+                      index + 1
+                    }.png`}
+                  />
+                  <p className="pokename">{item.name}</p>
+                </div>
+              );
+            })}
         </div>
       </div>
     );
